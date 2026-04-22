301cb4371d1283aa124e0a33596acb55f761babe
yu <ylu60@stuy.edu>
2026-04-21 23:55:25 -0400

    removed unused graphs

--- app/static/js/chart.js
+++ app/static/js/chart.js
@@ -50,66 +50,6 @@ const steamTheme = {
       },
       plugins: { legend: { labels: { color: 'white' } } }
     }
-  },
-  radar: {
-    colors: 'rgba(54, 162, 235, 0.4)',
-    borderColor: 'rgba(54, 162, 235, 1)',
-    options: {
-      maintainAspectRatio: false,
-      scales: {
-        r: {
-          angleLines: { color: 'rgba(255, 255, 255, 0.1)' },
-          grid: { color: 'rgba(255, 255, 255, 0.1)' },
-          pointLabels: { color: 'white' },
-          ticks: { backdropColor: 'transparent', color: 'white' }
-        }
-      },
-      plugins: { legend: { labels: { color: 'white' } } }
-    }
-  },
-  polarArea: {
-    colors: [
-      'rgba(255, 0, 55, 0.6)', 'rgba(0, 153, 255, 0.6)', 'rgba(255, 183, 0, 0.6)',
-      'rgba(59, 160, 160, 0.6)', 'rgba(108, 68, 187, 0.6)'
-    ],
-    options: {
-      maintainAspectRatio: false,
-      scales: {
-        r: {
-          grid: { color: 'rgba(255, 255, 255, 0.1)' },
-          ticks: { backdropColor: 'transparent', color: 'white' }
-        }
-      },
-      plugins: { legend: { labels: { color: 'white' } } }
-    }
-  },
-  scatter: {
-    colors: 'rgba(54, 162, 235, 0.8)',
-    borderColor: 'rgba(54, 162, 235, 1)',
-    options: {
-      maintainAspectRatio: false,
-      scales: {
-        y: { ticks: { color: 'white' }, grid: { color: 'rgba(255, 255, 255, 0.1)' } },
-        x: { ticks: { color: 'white' }, grid: { color: 'rgba(255, 255, 255, 0.1)' } }
-      },
-      plugins: { legend: { labels: { color: 'white' } } }
-    }
-  },
-  bubble: {
-    colors: 'rgba(54, 162, 235, 0.6)',
-    borderColor: 'rgba(54, 162, 235, 1)',
-    options: {
-      maintainAspectRatio: false,
-      scales: {
-        y: { ticks: { color: 'white' }, grid: { color: 'rgba(255, 255, 255, 0.1)' } },
-        x: { ticks: { color: 'white' }, grid: { color: 'rgba(255, 255, 255, 0.1)' } }
-      },
-      plugins: { legend: { labels: { color: 'white' } } }
-    }
-  },
-  default: {
-    colors: 'rgba(54, 162, 235, 0.6)',
-    options: { maintainAspectRatio: false }
   }
 };
 
@@ -149,8 +89,6 @@ async function fetchAndRenderChart(url, canvasId, chartType, labelName) {
     if (existingChart) {
       existingChart.destroy();
     }
-
-    // Pass formatted data to buildChart
     buildChart(canvasId, chartType, chartData.labels, chartData.data, labelName);
   } catch (error) {
     console.error(`Error loading data for ${canvasId}:`, error);
@@ -171,6 +109,8 @@ async function loadRecommendation() {
       titleImgLink.href = `https://store.steampowered.com/app/${randomTopGame.app_id}/`;
       const titleImgElement = document.getElementById("mostEngagingImg");
       titleImgElement.src = `https://steamcdn-a.akamaihd.net/steam/apps/${randomTopGame.app_id}/header.jpg`;
+
+      document.getElementById("recPublisher").innerText = game.publisher
     }
   } catch (error) {
     console.error("Error loading recommendation:", error);
--- app/static/js/trends.js
+++ app/static/js/trends.js
@@ -1,3 +1,4 @@
+
 const steamTheme = {
   doughnut: {
     colors: [
@@ -49,66 +50,6 @@ const steamTheme = {
       },
       plugins: { legend: { labels: { color: 'white' } } }
     }
-  },
-  radar: {
-    colors: 'rgba(54, 162, 235, 0.4)',
-    borderColor: 'rgba(54, 162, 235, 1)',
-    options: {
-      maintainAspectRatio: false,
-      scales: {
-        r: {
-          angleLines: { color: 'rgba(255, 255, 255, 0.1)' },
-          grid: { color: 'rgba(255, 255, 255, 0.1)' },
-          pointLabels: { color: 'white' },
-          ticks: { backdropColor: 'transparent', color: 'white' }
-        }
-      },
-      plugins: { legend: { labels: { color: 'white' } } }
-    }
-  },
-  polarArea: {
-    colors: [
-      'rgba(255, 0, 55, 0.6)', 'rgba(0, 153, 255, 0.6)', 'rgba(255, 183, 0, 0.6)',
-      'rgba(59, 160, 160, 0.6)', 'rgba(108, 68, 187, 0.6)'
-    ],
-    options: {
-      maintainAspectRatio: false,
-      scales: {
-        r: {
-          grid: { color: 'rgba(255, 255, 255, 0.1)' },
-          ticks: { backdropColor: 'transparent', color: 'white' }
-        }
-      },
-      plugins: { legend: { labels: { color: 'white' } } }
-    }
-  },
-  scatter: {
-    colors: 'rgba(54, 162, 235, 0.8)',
-    borderColor: 'rgba(54, 162, 235, 1)',
-    options: {
-      maintainAspectRatio: false,
-      scales: {
-        y: { ticks: { color: 'white' }, grid: { color: 'rgba(255, 255, 255, 0.1)' } },
-        x: { ticks: { color: 'white' }, grid: { color: 'rgba(255, 255, 255, 0.1)' } }
-      },
-      plugins: { legend: { labels: { color: 'white' } } }
-    }
-  },
-  bubble: {
-    colors: 'rgba(54, 162, 235, 0.6)',
-    borderColor: 'rgba(54, 162, 235, 1)',
-    options: {
-      maintainAspectRatio: false,
-      scales: {
-        y: { ticks: { color: 'white' }, grid: { color: 'rgba(255, 255, 255, 0.1)' } },
-        x: { ticks: { color: 'white' }, grid: { color: 'rgba(255, 255, 255, 0.1)' } }
-      },
-      plugins: { legend: { labels: { color: 'white' } } }
-    }
-  },
-  default: {
-    colors: 'rgba(54, 162, 235, 0.6)',
-    options: { maintainAspectRatio: false }
   }
 };
 
